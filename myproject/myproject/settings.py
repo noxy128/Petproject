00491479b880665eef0d17dcd438744a9f151f8d
noxy128 <trahatelhurok1488@gmail.com>
2025-08-17 17:31:36 -0700

    added dockerfiles

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',  # <-- Имя нашего сервиса из docker-compose.yml
+        'PORT': 5432,
     }
 }
 
